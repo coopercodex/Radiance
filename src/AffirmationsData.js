56c5b8f68685e9c18cfbaa98b9915cdf2849e4e7
Carissa Gross <carissamgross@gmail.com>
2022-10-29 10:14:16 -0600

    Create sample data, add file organization and images

---
+++ src/AffirmationsData.js
@@ -0,0 +1,10 @@
+const radianceData = {
+    'affirmations': [
+        {'id': 1, 'description': 'I forgive myself and set myself free.', 'image': 'src/images/image_1.png' },
+        {'id': 2, 'description': 'Breathing in, I send myself love.', 'image': 'src/images/image_2.png' },
+        {'id': 3, 'description': 'I am in the process of becoming the best version of myself.', 'image': 'src/images/image_3.png' },
+        {'id': 4, 'description': 'My possibilities are endless.', 'image': 'src/images/image_4.png' },
+        {'id': 5, 'description': 'I am enough.', 'image': 'src/images/image_5.png'}
+    ]
+}
+export default radianceData
