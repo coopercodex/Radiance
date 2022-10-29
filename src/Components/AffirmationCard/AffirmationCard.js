91492f9fcf5e926d505a327d079cdc88f4dca25a
Carissa Gross <carissamgross@gmail.com>
2022-10-29 12:26:00 -0600

    Update additional components and styling

--- src/Components/AffirmationCard/AffirmationCard.js
+++ src/Components/AffirmationCard/AffirmationCard.js
@@ -1,3 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Footer from './Components/Footer/Footer'
 import './AffirmationCard.css'
 
+class AffirmationCard extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className='affirmation-card'>
+                <p>Affirmation</p>
+                <img>IMAGE HERE</img>
+                <img>Favorites ICON</img>
+                <Footer />
+            </div>
+        )
+    }
+}
+export default AffirmationCard
