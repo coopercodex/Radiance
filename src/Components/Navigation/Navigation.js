6f37caf703e93850ebb27fe3358f7ca32f664f85
Carissa Gross <carissamgross@gmail.com>
2022-10-30 10:58:06 -0600

    Update css styling on affirmation card and app.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -5,7 +5,15 @@ import './Navigation.css'
 const Navigation = () => {
     return (
         <section className='Nav'>
-            <img className='radiance-logo' src={radiance_logo} />
+            <div className='logo-container'>
+                <img className='radiance-logo' src={radiance_logo} />
+            </div>
+            <div className='welcome-container'>
+                <h1 className='welcome'>
+                    hello, you lovely human
+                </h1>
+                <button className='affirmation-button'>take a breath</button>
+            </div>
         </section>
     )
 }
