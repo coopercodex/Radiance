6d8aaf970a892f30d5785732cf345861d1296922
Hunter Monroe <huntis@outlook.com>
2022-11-05 01:05:02 -0600

    Create favorite component styling and jsx

--- src/Components/AffirmationCard/AffirmationCard.js
+++ src/Components/AffirmationCard/AffirmationCard.js
@@ -5,12 +5,14 @@ import affirmationsData from '../../AffirmationsData'
 // import image_1 from '../../images/image_1.png'
 import favoriteLogo from '../../images/image_10.png'
 import { getData } from '../../apicalls';
+import Favorites from '../Favorites/Favorites';
 
 class AffirmationCard extends Component {
-    constructor({}) {
+    constructor() {
         super();
         this.state = {
-            affirmations:''
+            affirmations:'',
+            error: ''
         }
     
     }
@@ -35,9 +37,6 @@ class AffirmationCard extends Component {
         }
     }
  
-    generateRandom() {
-        return Math.floor(Math.random() * this.state.affirmations.length);
-    }
 
     render() {          
         console.log(this.state.affirmations)
@@ -46,20 +45,21 @@ class AffirmationCard extends Component {
                 <p>loading</p>
                 );
             }  
-         const affirm = this.state.affirmations[this.generateRandom()]
+         const affirm = this.state.affirmations[Math.floor(Math.random() * this.state.affirmations.length)]
         return (
-            <div className='card-container'>
-                <div className='affirmation-card'>
-                    {/* <p className='card-title'>Affirmation</p> */}
-                    <p className='card-description'> {affirm.description}</p>
-                    <img className='affirm-pic' src={affirm.image} />
-                    <div className='favorite-container'>
-                        <img className='favorite' src={favoriteLogo} />
-                    </div>
-                    {/* <Footer /> */}
-                </div>
-                <Footer handleReset={this.props.handleReset} />
-            </div>
+            // <div className='card-container'>
+            //     <div className='affirmation-card'>
+            //         <p className='card-title'>Affirmation</p>
+            //         <p className='card-description'> {affirm.description}</p>
+            //         <img className='affirm-pic' src={affirm.image} />
+            //         <div className='favorite-container'>
+            //             <img className='favorite' src={favoriteLogo} />
+            //         </div>
+            //     </div>
+                <div> <Favorites affirmation={this.state.affirmations}/> </div>
+                // <Footer handleReset={this.props.handleReset} />
+
+            // </div>
         )
     }
 }
