ac6900901a81878216ab85ebb744f5a8709cbdc7
Hunter Monroe <huntis@outlook.com>
2022-11-05 20:40:50 -0600

    Fix Responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './Footer.css'
 import radiance_logo from '../../images/radiance_logo.png'
-import { BrowserRouter, Link, Route, Routes, Switch } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import App from '../App/App'
 
-const Footer = ({ handleReset, handleFavoriteClick, resetFavorites }) => {
+const Footer = () => {
     return (
         <div className='footer'>
             <Link to="/" className="home-link">HOME</Link>
-            {/* <Route exact path='/' component={App}/> */}
-            {/* <Link to='/' onClick={() => this.getData()} className="home-button">Home</Link> */}
             <img className='radiance-logo-footer' src={radiance_logo} alt='Line art of sunset in the mountains'/>
             <Link to='/favorites' className='favorite-link'>FAVORITES </Link>
         </div>
