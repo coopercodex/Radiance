d9aaf25ed31ad5e9c630e912204982a61ee9858c
Derek Cooper <derekcooper20@gmail.com>
2022-11-06 17:00:46 -0700

    Adds fav button and user message

--- src/Components/AffirmationCard/AffirmationCard.js
+++ src/Components/AffirmationCard/AffirmationCard.js
@@ -1,8 +1,9 @@
 
-import React, { Component, useState } from 'react'
+import React from 'react'
 import Footer from '../Footer/Footer';
 import './AffirmationCard.css'
 import favoriteLogo from '../../images/image_10.png'
+import favoriteLogo2 from '../../images/image_11.png'
 
 const AffirmationCard = ({ affirmations, add }) => {
     const getAffirmations = affirmations.map((affirm) => {
@@ -18,7 +19,7 @@ const AffirmationCard = ({ affirmations, add }) => {
                 </p>
                 <img className='affirm-pic' src={currentAffirm.image} alt="Geometric line art of suns, moons, and stars" />
                 <div className='favorite-container'>
-                    <button onClick={() => add(currentAffirm.id)}> <img className='favorite-button' src={favoriteLogo} alt="Line art of a star" /></button>
+                    <button onClick={() => add(currentAffirm.id)}> <img src={favoriteLogo} onMouseOut={e => (e.currentTarget.src = favoriteLogo)} onMouseOver={e => (e.currentTarget.src = favoriteLogo2)} className='favorite-button' alt="Line art of a star" /></button>
                 </div>
             </div>
             <Footer />
